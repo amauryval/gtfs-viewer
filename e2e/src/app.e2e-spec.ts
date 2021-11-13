a437d06a09d7133da46069ead8ac17852a576cfe
amaury5432 <amauryval@gmail.com>
2021-11-13 12:16:01 +0100

    rnm (*) gtfs-viewer-front to gtfs-viewer

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('gtfs-viewer-front app is running!');
+    expect(await page.getTitleText()).toEqual('gtfs-viewer app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gtfs-viewer-front'`, () => {
+  it(`should have as title 'gtfs-viewer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gtfs-viewer-front');
+    expect(app.title).toEqual('gtfs-viewer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gtfs-viewer-front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gtfs-viewer app is running!');
   });
 });
