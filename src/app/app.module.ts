a2df5be6a4e2b12e2e87cc41610420fcd23e05c8
amaury5432 <amauryval@gmail.com>
2021-11-13 12:10:34 +0100

    upd (app.module.ts) fr local added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -34,6 +34,7 @@ registerLocaleData(localeFr);
   ],
   providers: [
     MapService,
+    {provide: LOCALE_ID, useValue: 'fr'}
   ],
   bootstrap: [AppComponent]
 })
